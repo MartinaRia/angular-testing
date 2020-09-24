3d32e8041f1f0ef9dd8bccab535bb3338c7714f6
Martina <martina.ria@hotmail.com>
2020-09-24 23:37:06 +0200

    App setup and step1:https://angular.io/tutorial/toh-pt1 | completed

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'; // sostituisce temporaneamente un database
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+// first implementation
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
+
+// export class HeroesComponent implements OnInit {
+//
+//   heroes = HEROES;
+//
+//   constructor() { }
+//
+//   ngOnInit() { }
+// }
